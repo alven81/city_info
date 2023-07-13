c48db2edbb4a6eecf67f73c8b2a26dbb3bbb6bd7
Aliaksandr Kot <aliaksandr.a.kot@tietoevry.com>
2023-07-13 15:01:11 +0200

    Create entity classes

---
+++ CityInfo.API/Entities/PointOfInterest.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace CityInfo.API.Entities
+{
+    public class PointOfInterest
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public City? City { get; set; }
+
+        public PointOfInterest(string name) 
+        {
+            Name = name;
+        }
+    }
+}
