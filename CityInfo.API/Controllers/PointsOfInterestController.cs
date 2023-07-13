918cf2b00583ab5baa85c9a6e07783a6434de6ed
Aliaksandr Kot <aliaksandr.a.kot@tietoevry.com>
2023-07-13 11:36:42 +0200

    Replacing default logger and logging to file

--- CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -21,6 +21,7 @@ namespace CityInfo.API.Controllers
         {
             try
             {
+                //throw new Exception("Exception sample.");
 
                 var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
 
--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -1,8 +1,20 @@
 using Microsoft.AspNetCore.StaticFiles;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .WriteTo.File("logs/cityinfo.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Logging.ClearProviders();
-builder.Logging.AddConsole();
+
+// Disable builtIn logger
+//builder.Logging.ClearProviders();
+//builder.Logging.AddConsole();
+
+// Enable custom logger
+builder.Host.UseSerilog();
 
 // Add services to the container.
 
